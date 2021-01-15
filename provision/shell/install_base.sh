b05fabc39082d55c465dc8f0eb98b28bafda205f
MMarugan <MMGMarugan@hotmail.com>
2021-01-15 10:31:22 +0100

    Replaces image to fedora-coreos-preview

--- provision/shell/install_base.sh
+++ provision/shell/install_base.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-echo "Empty provisioning script"
+# echo "Install base packages"
+# yum install -y wget git net-tools bind-utils yum-utils iptables-services bridge-utils bash-completion kexec-tools sos psacct
+
+# # yum update
+# # reboot
+
+# # echo "Install EPEL repo"
+# # yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# # Generates a conflict in Fedora 33
+
+# yum -y install ansible pyOpenSSL
+
+# git clone https://github.com/openshift/openshift-ansible
+# cd openshift-ansible
+# git checkout release-3.11
+
+# yum -y install docker-1.13.1
+# rpm -V docker-1.13.1
+# docker version
