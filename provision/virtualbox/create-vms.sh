2c9903e30b08beb096bd74de3e7f42d2ec5dd12b
MMarugan <MMGMarugan@hotmail.com>
2021-01-29 20:27:11 +0100

    Adds support for windows to create FedoraCoreOS machines

--- provision/virtualbox/create-vms.sh
+++ provision/virtualbox/create-vms.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-ISO_BASE_PATH="/home/manu/Downloads/"
+uname -r | grep -qe "Microsoft$"
+if [ $? -eq 0 ]; then
+  echo "Windows System found"
+  VBOXMANAGE="/mnt/c/Program Files/Oracle/VirtualBox/VBoxManage.exe"
+  ISO_BASE_PATH="D:/OKD/ISO/"
+  VM_BASE_PATH="D:/OKD/VMs/"
+else
+  VBOXMANAGE="VBoxManage"
+  ISO_BASE_PATH="~/Downloads/"
+fi
+
+PFSENSE_HOST="okd4-pfsense"
+PFSENSE_ISO="${ISO_BASE_PATH}pfSense-CE-2.4.5-RELEASE-p1-amd64.iso"
+PFSENSE_MAC="001122334499"
+
 
 declare -A HOSTS=(
   ["okd4-bootstrap"]="fedora-coreos-32.20200715.3.0-live.x86_64-bootstrap.iso"
@@ -20,33 +34,63 @@ declare -A MACS=(
   ["okd4-compute-2"]="001122334405"
 )
 
+# # Create pfsense host
+#   host="${PFSENSE_HOST}"
+#   "${VBOXMANAGE}" createvm --name "${host}" --ostype Fedora_64 --register
+#   # ---
+#   "${VBOXMANAGE}" modifyvm "${host}" --memory 1024  --cpus 1 --longmode on --apic on
+#   "${VBOXMANAGE}" modifyvm "${host}" --vram 16 --graphicscontroller vmsvga
+#   "${VBOXMANAGE}" modifyvm "${host}" --audio none
+#   # ---
+#   NETNAME=$("${VBOXMANAGE}" list -l hostonlyifs | grep 192.168.61.1 -B 3 | grep Name | awk '{print $2}')
+#   "${VBOXMANAGE}" modifyvm "${host}" --nic1 hostonly --hostonlyadapter1 "${NETNAME}" --nictype1 Am79C970A --macaddress1 "${PFSENSE_MAC}"
+#   "${VBOXMANAGE}" modifyvm "${host}" --nic2 nat --nictype2 Am79C970A
+#   # ---
+#   "${VBOXMANAGE}" createmedium disk --filename "${VM_BASE_PATH}${host}-disk0.vdi" --size 8192 --variant Standard
+#   DISKUUID=$("${VBOXMANAGE}" list hdds | grep "${host}-disk0.vdi" -B 4 | grep "^UUID:" | awk '{print $2}')
+#   "${VBOXMANAGE}" storagectl "${host}" --name "SATA" --add sata --controller IntelAHCI --portcount 1 --bootable on
+#   "${VBOXMANAGE}" storageattach "${host}" \
+#                            --storagectl "SATA" \
+#                            --device 0 \
+#                            --port 0 \
+#                            --type hdd \
+#                            --medium "${VM_BASE_PATH}${host}-disk0.vdi"
+#   # ---
+#   "${VBOXMANAGE}" storagectl "${host}" --name "IDE" --add ide
+#   "${VBOXMANAGE}" storageattach "${host}" --storagectl "IDE" --port 0  --device 0 --type dvddrive --medium "${PFSENSE_ISO}"
+#   # ---
+#   "${VBOXMANAGE}" modifyvm "${host}" --boot1 disk --boot2 dvd --boot3 floppy
+
+
+# Create fedora hosts
 for host in "${!HOSTS[@]}"; do
   ISO="${ISO_BASE_PATH}${HOSTS[$host]}"
   MAC="${MACS[$host]}"
 
   echo "------ ${host} - ${ISO} ------"
 
-  VBoxManage createvm --name "${host}" --ostype Fedora_64 --register
+  "${VBOXMANAGE}" createvm --name "${host}" --ostype Fedora_64 --register
   # ---
-  VBoxManage modifyvm "${host}" --memory 4096 --cpus 1
-  VBoxManage modifyvm "${host}" --vram 16 --graphicscontroller vmsvga
-  VBoxManage modifyvm "${host}" --audio none
+  "${VBOXMANAGE}" modifyvm "${host}" --memory 4096 --cpus 2 --longmode on --apic on
+  "${VBOXMANAGE}" modifyvm "${host}" --vram 16 --graphicscontroller vmsvga
+  "${VBOXMANAGE}" modifyvm "${host}" --audio none
   # ---
-  NETNAME=$(VBoxManage list -l hostonlyifs | grep 192.168.61.1 -B 3 | grep Name | awk '{print $2}')
-  VBoxManage modifyvm "${host}" --nic1 hostonly --hostonlyadapter1 "${NETNAME}" --nictype1 Am79C970A --macaddress1 "${MAC}"
+  NETNAME=$("${VBOXMANAGE}" list -l hostonlyifs | grep 192.168.61.1 -B 3 | grep Name | awk '{print $2}')
+  "${VBOXMANAGE}" modifyvm "${host}" --nic1 hostonly --hostonlyadapter1 "${NETNAME}" --nictype1 Am79C970A --macaddress1 "${MAC}"
   # ---
-  VBoxManage createmedium disk --filename "${host}"/disk0.vdi --size 8192 --variant Standard
-  DISKUUID=$(VBoxManage list hdds | grep "\/${host}\/disk0.vdi" -B 4 | grep "^UUID:" | awk '{print $2}')
-  vboxmanage storagectl "${host}" --name "SATA" --add sata --controller IntelAHCI --portcount 1 --bootable on
-  VBoxManage storageattach "${host}" \
+  "${VBOXMANAGE}" createmedium disk --filename "${VM_BASE_PATH}${host}-disk0.vdi" --size 8192 --variant Standard
+  DISKUUID=$("${VBOXMANAGE}" list hdds | grep "${host}-disk0.vdi" -B 4 | grep "^UUID:" | awk '{print $2}')
+  "${VBOXMANAGE}" storagectl "${host}" --name "SATA" --add sata --controller IntelAHCI --portcount 1 --bootable on
+  "${VBOXMANAGE}" storageattach "${host}" \
                            --storagectl "SATA" \
                            --device 0 \
                            --port 0 \
                            --type hdd \
-                           --medium "${host}"/disk0.vdi
+                           --medium "${VM_BASE_PATH}${host}-disk0.vdi"
   # ---
-  VBoxManage storagectl "${host}" --name "IDE" --add ide
-  VBoxManage storageattach "${host}" --storagectl "IDE" --port 0  --device 0 --type dvddrive --medium "${ISO}"
+  "${VBOXMANAGE}" storagectl "${host}" --name "IDE" --add ide
+  "${VBOXMANAGE}" storageattach "${host}" --storagectl "IDE" --port 0  --device 0 --type dvddrive --medium "${ISO}"
   # ---
-  VBoxManage modifyvm "${host}" --boot1 disk --boot2 dvd --boot3 floppy
+  "${VBOXMANAGE}" modifyvm "${host}" --boot1 disk --boot2 dvd --boot3 floppy
 done
+
--- provision/virtualbox/delete-vms.sh
+++ provision/virtualbox/delete-vms.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+uname -r | grep -qe "Microsoft$"
+if [ $? -eq 0 ]; then
+  VBOXMANAGE="/mnt/c/Program Files/Oracle/VirtualBox/VBoxManage.exe"
+  echo "Windows System found"
+else
+  VBOXMANAGE="VBoxManage"
+fi
+
 HOSTS="okd4-bootstrap okd4-control-plane-1 okd4-control-plane-2 okd4-control-plane-3 okd4-compute-1 okd4-compute-2"
 
 for host in $(echo ${HOSTS}); do
   echo ${host}
-  VBoxManage unregistervm "${host}" --delete
+  "${VBOXMANAGE}" unregistervm "${host}" --delete
 done
